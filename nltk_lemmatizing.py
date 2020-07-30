3f48d8a5690279049d18bb72618a81e0b566a31c
Eric Oliverio <ericoliv@buffalo.edu>
2020-07-30 14:12:17 -0400

    Delete nltk_lemmatizing.py

--- nltk_lemmatizing.py
+++
@@ -1,8 +0,0 @@
-from nltk.stem import WordNetLemmatizer
-
-lemmatizer = WordNetLemmatizer()
-
-#pos = part of speech 'a' = adverb, default = 'n' /noun
-print(lemmatizer.lemmatize('better',pos ='a'))
-print(lemmatizer.lemmatize('best',pos ='a'))
-print(lemmatizer.lemmatize('run',pos ='a'))
